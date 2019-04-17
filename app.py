d13b58c8de81172f589f0f3e8c6559fed895edd3
David Golke <c-david.golke@charter.com>
2019-04-17 13:48:55 -0600

    Lecture: Creating our application endpoints

--- app.py
+++ app.py
@@ -2,8 +2,44 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/') # 'http://www.google.com/'
-def home():
-    return "Hello, world!"
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+# POST - used to receive data
+# GET - used to send data back only
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>', methods=['GET']) #   'http://127.0.0.1:5000/store/some_name'
+def get_store(name):
+    pass
+
+# GET /store
+@app.route('/store', methods=['GET'])
+def get_stores(name):
+    pass
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
 
 app.run(port=5000)
