ce46940ba9194aa8b3aa789c03132b9d007e7a1f
David Golke <golkedj@gmail.com>
2019-05-08 16:08:53 -0600

    Lecture: A recap of the code + a few changes!

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'dgdevelopment'
 api = Api(app)
 
@@ -20,11 +21,15 @@ jwt = JWT(app, authenticate, identity)  # /auth
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores/')
-
+api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
+
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+
     app.run(port=5000, debug=True)
--- resources/user.py
+++ resources/user.py
@@ -1,19 +1,19 @@
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
-class UserRegister(Resource):
 
+class UserRegister(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('username',
-        type=str,
-        required=True,
-        help="This field cannot be blank."
-    )
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
     parser.add_argument('password',
-        type=str,
-        required=True,
-        help="This field cannot be blank."
-    )
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
 
     def post(self):
         data = UserRegister.parser.parse_args()
@@ -23,6 +23,5 @@ class UserRegister(Resource):
 
         user = UserModel(**data)
         user.save_to_db()
-        
+
         return {"message": "User created successfully."}, 201
-        
